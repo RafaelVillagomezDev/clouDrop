752c757024c97094a19049aaf13316547686e211
RafaelVillagomezDev <rafaelvillagomez.dev@gmail.com>
2022-04-21 12:34:25 +0200

    Eliminacion de Componente Counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+      <h1>Hii</h1>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -50,7 +51,7 @@ function App() {
             React Redux
           </a>
         </span>
-      </header>
+      </header> */}
     </div>
   );
 }
