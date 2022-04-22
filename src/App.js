12d099e1e9ed0b5db19803dd4c9654a183d40fa6
RafaelVillagomezDev <rafaelvillagomez.dev@gmail.com>
2022-04-22 15:39:24 +0200

    Creacion Navbar y modificacion estilos

--- src/App.js
+++ src/App.js
@@ -1,59 +1,24 @@
-import React from 'react';
+import React from 'react'
 // import logo from './logo.svg';
 // import { Counter } from './features/counter/Counter';
-import './App.css';
+
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/Home/Home'
+// import { ThemeProvider } from '@mui/material/styles'
+//Config theme Material Mui global
+// import theme from './themeConfig';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hii</h1>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit FUNCIONA
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header> */}
-    </div>
-  );
+    <>
+      {/* <ThemeProvider theme={theme}> */}
+        <Navbar />
+        <Home />
+        {/*<Counter /> */}
+      {/* </ThemeProvider> */}
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/themeConfig.js
@@ -0,0 +1,14 @@
+import {createTheme} from '@mui/material/styles'
+
+const theme=createTheme({
+
+ palette:{
+     primary:{
+          main:'#fffcf9'
+     },
+    
+ }
+
+})
+
+export default theme
